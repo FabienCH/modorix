4d56bd6756a25ada286555ddc1617022dab59edc
Fabien Charvaz <fabien.charvaz@photoweb.fr>
2024-05-30 19:42:52 +0200

    ci: init ci

--- apps/modorix-backend/src/adapters/block-user.controller.spec.ts
+++ apps/modorix-backend/src/adapters/block-user.controller.spec.ts
@@ -1,22 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { BlockUserController } from './block-user.controller';
 import { BlockUserService } from '../domain/block-user.service';
+import { BlockUserRepository } from '../infrastructure/block-user.repository';
+import { BlockUserController } from './block-user.controller';
 
 describe('BlockUserController', () => {
   let blockUserController: BlockUserController;
+  let blockUserRepository: BlockUserRepository;
+  let blockUserSpy: jest.SpyInstance;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [BlockUserController],
-      providers: [BlockUserService],
+      providers: [BlockUserService, BlockUserRepository],
     }).compile();
 
     blockUserController = app.get<BlockUserController>(BlockUserController);
+    blockUserRepository = app.get<BlockUserRepository>(BlockUserRepository);
+    blockUserSpy = jest.spyOn(blockUserRepository, 'blockUser');
   });
 
   describe('Block X user', () => {
     it('should block a X user"', () => {
-      expect(blockUserController.blockUser({ id: '1', blockedAt: '2024-05-27T18:01:45Z' })).toHaveBeenCalled()
+      blockUserController.blockUser({ id: '1', blockedAt: '2024-05-27T18:01:45Z' });
+      expect(blockUserSpy).toHaveBeenCalledWith({ id: '1', blockedAt: '2024-05-27T18:01:45Z' });
     });
   });
 });
