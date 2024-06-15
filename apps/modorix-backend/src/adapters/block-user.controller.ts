b5adebd02aeeeb29914252a732d2d571ccc35468
Fabien Charvaz <fabien.charvaz@photoweb.fr>
2024-06-15 09:34:15 +0200

    feat(block-users): add error handling when wrong block reason is given

--- apps/modorix-backend/src/adapters/block-user.controller.ts
+++ apps/modorix-backend/src/adapters/block-user.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Get, HttpCode, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Get, HttpCode, HttpException, HttpStatus, Post } from '@nestjs/common';
 import { BlockUsersService } from '../domain/block-user.service';
+import { BlockReasonError } from '../domain/errors/block-reason-error';
 import { BlockXUserRequestDto, XUserDto } from './x-user-dto';
 
 @Controller()
@@ -9,7 +10,14 @@ export class BlockUsersController {
   @Post('block-users')
   @HttpCode(201)
   blockUser(@Body() user: BlockXUserRequestDto): void {
-    return this.blockUsersService.blockUser(user);
+    try {
+      return this.blockUsersService.blockUser(user);
+    } catch (error) {
+      if (error instanceof BlockReasonError) {
+        throw new BadRequestException(error.message);
+      }
+      throw new HttpException('An unexpected error occurred', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   @Get('block-users')
